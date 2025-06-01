b51bb6f28207abb0817a0ae1b48766d1e985be7a
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-06-01 12:56:08 +0600

    Add files via upload

---
+++ Chapter 1. Basic Math and Calculus Review/Exercise/Example 1-19. A derivative calculator in Python.py
@@ -0,0 +1,9 @@
+def f(x):
+    return x**2
+def dx_f(x):
+    return 2*x
+# This is a simple derivative calculator using Python functions
+# It defines a function f(x) = x^2 and its derivative dx_f(x) = 2x
+slope_at_2 = dx_f(2.0)
+# This calculates the derivative of f at x=2
+print(slope_at_2) # prints 4.0
